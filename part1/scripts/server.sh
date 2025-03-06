d392e9399c972c4eb5a1e2cb2e545e37e0eaa2e4
nyzs <oknakoca@gmail.com>
2025-03-06 21:18:50 +0100

    setup script for controller and agent

--- part1/scripts/server.sh
+++ part1/scripts/server.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
+echo "SERVER_IP: ${SERVER_IP}"
+echo "SERVER_WORKER_IP: ${SERVER_WORKER_IP}"
+
 curl -sfL https://get.k3s.io | sh -
+
 
 mkdir -p /vagrant_shared
 
@@ -6,3 +12,4 @@ touch /vagrant_shared/token
 
 cat /var/lib/rancher/k3s/server/token > /vagrant_shared/token
 
+echo "k3s server installed"
--- part1/scripts/server_worker.sh
+++ part1/scripts/server_worker.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
 
 echo "Installing k3s worker"
 
-cat /vagrant_shared/token
+echo "SERVER_IP: ${SERVER_IP}"
+echo "SERVER_WORKER_IP: ${SERVER_WORKER_IP}"
+
+TOKEN=$(cat /vagrant_shared/token)
+
+curl -sfL https://get.k3s.io | K3S_URL=https://${SERVER_IP}:6443 K3S_TOKEN=${TOKEN} sh -
+
+echo "k3s worker installed"
